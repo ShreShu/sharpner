da900411344e513b52b3eb35050272d85a9e37ca
ShreShu <66847673+ShreShu@users.noreply.github.com>
2022-06-14 15:44:23 +0530

    Update appointment.js

--- appointment.js
+++ appointment.js
@@ -1,14 +1,33 @@
+myForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  var n = document.getElementById("name");
+  var e = document.getElementById("email");
+  container.innerHTML = "";
+  axios
+    .post("https://crudcrud.com/api/fbeb67f713f048c89d57f851b4c77102/users", {
+      name: n.value,
+      email: e.value,
+    })
+    .then(
+        location.reload()
+    )
+    .catch((error) => {console.log(error);});
+   n.value = "";
+   e.value = "";
 
-myForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    var n = document.getElementById('name');
-    var e = document.getElementById('email');
+  //console.log(myObj_Serialized);
+});
 
-axios.post("https://crudcrud.com/api/fbeb67f713f048c89d57f851b4c77102/users",{
-    name:n.value,
-    email:e.value
-})
-    n.value = '';
-    e.value = ''
 
-})
+function showOutput(res) {
+  res.forEach((user) => {
+    let email = user.email;
+    container.innerHTML += `
+    <p>name : ${user.name}
+    email: ${user.email}
+    <button id="delete" onclick="deleteUser('${email}')">Delete</button>
+    <button id="edit" onclick="editUser('${user}'}')">Edit</button>
+    </p>
+`;
+  });
+}
