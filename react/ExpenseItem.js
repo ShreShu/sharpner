613e846bbeced0351b3b2bca24a9d6c1761f52ba
ShreShu <66847673+ShreShu@users.noreply.github.com>
2022-06-25 15:43:28 +0530

    Adding delete button

--- react/ExpenseItem.js
+++ react/ExpenseItem.js
@@ -2,11 +2,21 @@ import "./ExpenseItem.css";
 import ExpenseDetails from "./ExpenseDetails";
 import ExpenseDate from "./ExpenseDate";
 const ExpenseItem = (props) => {
+  const deleted = () => {
+    console.log("deleted");
+  };
   return (
     <div>
       <h1> Expense Items</h1>
       <ExpenseDetails amount={props.amount} />
       <ExpenseDate date={props.date} />
+
+      <div>
+        <button onClick={deleted}>Delete</button>
+        {/* we have to pass only the name of function ie we are poitning to that function here not with parenthesis ,if we use delete() then JS will execute it when the onClick event is parsed
+     and that will be parsed when the JSX is returned
+     */}
+      </div>
     </div>
   );
 };
