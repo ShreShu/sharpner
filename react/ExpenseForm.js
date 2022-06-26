b1b7747bb3aceb955b7cc1461437b2e616cb6a8e
ShreShu <66847673+ShreShu@users.noreply.github.com>
2022-06-26 16:51:29 +0530

    Update ExpenseForm.js

--- react/ExpenseForm.js
+++ react/ExpenseForm.js
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const ExpenseForm = () => {
-  const showExpenseChange = (e) => {
-    console.log(e.target.value);
+  const [enteredtitle, setTitle] = useState("");
+  const [enteredAmount, setAmount] = useState("");
+  const [enteredDate, setDate] = useState("");
+
+  const showExpenseTitle = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const showExpenseAmount = (e) => {
+    setAmount(e.target.value);
+  };
+
+  const showExpensedate = (e) => {
+    setDate(e.target.value);
   };
 
   return (
     <div>
       <form>
         <label htmlFor="title">Expense Title</label>
-        <input type="text" id="title" onChange={showExpenseChange} />
+        <input type="text" id="title" onChange={showExpenseTitle} />
 
         <label htmlFor="title">Expense Amount</label>
-        <input type="number" id="amount" onChange={showExpenseChange} />
+        <input type="number" id="amount" onChange={showExpenseAmount} />
 
         <label htmlFor="title">Expense Title</label>
-        <input type="date" id="date" onChange={showExpenseChange} />
+        <input type="date" id="date" onChange={showExpensedate} />
       </form>
 
       <button>Add</button>
