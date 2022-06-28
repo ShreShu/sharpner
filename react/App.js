18223dbdabd5e834a01e839b7c74ca803d6e0488
ShreShu <66847673+ShreShu@users.noreply.github.com>
2022-06-28 16:17:21 +0530

    Ui changes
    Ui changes to show if there is no expense or only 1

--- react/App.js
+++ react/App.js
@@ -31,20 +31,34 @@ function App() {
 
       <ExpenseFilter selected={filteredYear} onChangeFilter={filterHandler} />
 
-      {filteredExpenses.map((expense) => {
-        return (
-          <ExpenseItem
-            key={Math.random()}
-            /*if we don't use key, to react all the items will look similar
-            it only sees that the array has changed and then add the new one to the array and walk through all the divs and channge the contect of all divs to match array content 
-            so new value will be there but performance wise its not good as all the element are visited and updated  
-            */
-            date={expense.expenseDate}
-            amount={expense.expenseAmount}
-            title={expense.expenseTitle}
-          />
-        );
-      })}
+      {filteredExpenses.length === 0 ? (
+        <p>No expense</p>
+      ) : filteredExpenses.length === 1 ? (
+        filteredExpenses.map((expense) => {
+          return (
+            <>
+              <ExpenseItem
+                key={Math.random()}
+                date={expense.expenseDate}
+                amount={expense.expenseAmount}
+                title={expense.expenseTitle}
+              />
+              <p>only one expense here, please add more</p>
+            </>
+          );
+        })
+      ) : (
+        filteredExpenses.map((expense) => {
+          return (
+            <ExpenseItem
+              key={Math.random()}
+              date={expense.expenseDate}
+              amount={expense.expenseAmount}
+              title={expense.expenseTitle}
+            />
+          );
+        })
+      )}
     </Card>
   );
 }
